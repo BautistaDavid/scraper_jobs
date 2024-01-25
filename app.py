cb8f706b7f29d7987a0a3db3fc8d049ea8b1915f
David Bautista <dfbau2002@gmail.com>
2024-01-24 22:37:18 -0500

    app

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st 
+from scraper import funcion_scraper
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+
+local_css('style.css')
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+
+bienvenida = """
+Bienvenido a la APP "Scraper de trabajos." . Esta APP es un pequeño ejercicio que 
+implementa un scraper para realizar una búsqueda de cualquier término deseado en el
+portal de empleo "El Empleo", además de eso logra filtrar los resultados por algún 
+término en específico. La idea nació después de que una persona especial para mí 
+se enfrentara al proceso de búsqueda de prácticas laborales, proceso el cual, por 
+experiencia propia sé que puede llegar a ser frustrante, de modo que la finalidad 
+de la herramienta es distraerse, explorando de otra manera información de un portal
+de empleo. 
+"""
+
+st.title('Scraper de Trabajos. 📌')
+
+st.text(bienvenida)
+
+icon("search")
+st.text('Término de búsqueda:')
+selected = st.text_input("",'Escribe tu búsqueda acá. Ej: "Prácticas economía".')
+st.text('Término de filtrado')
+filter = st.text_input("",'Escriba el término por el cual desea filtrar. Ej: "python". Después presiona "Ok"')
+button_clicked = st.button("Ok")
+
+
+
+
+if button_clicked:
+    st.dataframe(funcion_scraper(selected,filter))
+else:
+    None
--- scraper.py
+++ scraper.py
@@ -1,33 +1,47 @@
 from bs4 import BeautifulSoup
 import requests as rq
 import pandas as pd
+import streamlit as st
 
-termino = input('Ingrese el termino que quiere Buscar:')
-termino = termino.replace(' ','%20')
+def busqueda_termino(termino_filtro,df):
 
-portal_empleo = f"https://www.elempleo.com/co/ofertas-empleo?&trabajo{termino}"
-pag_ = rq.get(portal_empleo)
+    tabla = df[df['info'].str.lower().str.contains(termino_filtro)]
+    st.write(f'La tabla resultado tiene {tabla.shape[0]} registros')
+    return tabla
 
-pag_portal = BeautifulSoup(pag_.text,'lxml')
+def funcion_scraper(termino,termino_filtro):
+    st.write('Estamos haciendo el scraping... En un segundo tendra sus resultados.')
 
-titulos = []
-links = []
-info = []
+    # termino = input('Ingrese el termino que quiere Buscar:')
+    termino = termino.replace(' ','%20')
 
-for publicacion in pag_portal.find_all('h2',attrs={'class':'h4 item-title ee-mod'}):
-    titulos.append(publicacion.a.get('title'))
-    links.append('https://www.elempleo.com' + publicacion.a.get('href'))
+    portal_empleo = f"https://www.elempleo.com/co/ofertas-empleo?&trabajo={termino}"
+    pag_ = rq.get(portal_empleo)
 
-resultados = pd.DataFrame()
-resultados['Titulos'] = titulos
-resultados['link'] = links
+    pag_portal = BeautifulSoup(pag_.text,'lxml')
 
-for sub_pub in resultados['link']:
-    pag = BeautifulSoup(rq.get(sub_pub).text,'lxml')
-    info.append(pag.find_all('div',attrs={'class':'description-block'})[0].find('span').text.strip())
-resultados['info'] = info
+    titulos = []
+    links = []
+    info = []
 
-resultados.to_csv('test.csv',index=False)
-print(resultados.head(2))
+    for publicacion in pag_portal.find_all('h2',attrs={'class':'h4 item-title ee-mod'}):
+        titulos.append(publicacion.a.get('title'))
+        links.append('https://www.elempleo.com' + publicacion.a.get('href'))
+
+    resultados = pd.DataFrame()
+    resultados['Titulos'] = titulos
+    resultados['link'] = links
+
+    for sub_pub in resultados['link']:
+        pag = BeautifulSoup(rq.get(sub_pub).text,'lxml')
+        info.append(pag.find_all('div',attrs={'class':'description-block'})[0].find('span').text.strip())
+    resultados['info'] = info
+
+    # resultados.to_csv('test.csv',index=False)
+    # print(resultados.head(2))
+
+    return busqueda_termino(termino_filtro,resultados)
+
+
 
 
